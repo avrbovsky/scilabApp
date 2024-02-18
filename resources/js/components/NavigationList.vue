3bd27dc395b0e654ef6a06dc42680e25f73511d9
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-02-18 19:49:59 +0100

    Created main layout

---
+++ resources/js/components/NavigationList.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-list density="compact" variant="tonal" nav class="flex flex-col">
+        <v-list-item
+            prepend-icon="mdi-folder"
+            title="My Files"
+            value="myfiles"
+        ></v-list-item>
+        <v-list-item
+            prepend-icon="mdi-account-multiple"
+            title="Shared with me"
+            value="shared"
+        ></v-list-item>
+        <v-list-item
+            prepend-icon="mdi-star"
+            title="Starred"
+            value="starred"
+        ></v-list-item>
+        <v-list-item
+            class="justify-self-end"
+            prepend-icon="mdi-logout"
+            :title="$t('Logout')"
+            value="logout"
+        >
+        </v-list-item>
+    </v-list>
+</template>
--- resources/js/layouts/MainLayout.vue
+++ resources/js/layouts/MainLayout.vue
@@ -1,8 +1,19 @@
+<script setup>
+import NavigationList from "../components/NavigationList.vue";
+</script>
+
 <template>
-    <v-navigation-drawer>
+    <v-app-bar color="primary" title="Application bar"></v-app-bar>
+    <v-navigation-drawer expand-on-hover rail>
         <v-list>
-            <v-list-item title="Navigation Drawer"></v-list-item>
+            <v-list-item
+                prepend-avatar="https://randomuser.me/api/portraits/men/73.jpg"
+                title="John Doe"
+                subtitle="john_doe@example.com"
+            ></v-list-item>
         </v-list>
+        <v-divider></v-divider>
+        <NavigationList />
     </v-navigation-drawer>
     <v-main>
         <router-view></router-view>
