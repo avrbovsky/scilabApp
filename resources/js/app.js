cab9be9ba9d1fc025209a01ea9167374f0eae659
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-02-18 18:58:56 +0100

    Added fallback language

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,20 @@ app.use(router);
 app.use(i18nVue, {
     resolve: async (lang) => {
         const langs = import.meta.glob("../../lang/*.json");
-        return await langs[`../../lang/${lang}.json`]();
+
+        try {
+            const langModule = await langs[`../../lang/${lang}.json`]();
+            return langModule;
+        } catch (error) {
+            console.error(
+                `Could not load ${lang}, falling back to default language.`
+            );
+            const fallbackLang = "en";
+            const fallbackLangModule = await langs[
+                `../../lang/${fallbackLang}.json`
+            ]();
+            return fallbackLangModule;
+        }
     },
 });
 app.mount("#app");
