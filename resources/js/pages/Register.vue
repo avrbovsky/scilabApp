61ac90fbe347ec2c26486805b74361d39c6c9e17
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-02-19 14:50:25 +0100

    Added form validation to Register View

--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -1,9 +1,46 @@
 <script setup>
+import { reactive, ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+const valid = ref(false);
+const form = ref(null);
+const formState = reactive({
+    username: "",
+    email: "",
+    password: "",
+    passwordRepeat: "",
+});
+
+const usernameRules = [(value) => !!value || "Username is required"];
+
+const emailRules = [
+    (value) => !!value || "Email is required",
+    (value) => /.+@.+\..+/.test(value) || "Email must be valid",
+];
+
+const passwordRules = [
+    (value) => !!value || "Password is required",
+    (value) =>
+        (value && value.length >= 6) ||
+        "Password must be at least 6 characters long",
+];
+
+const passwordRepeatRules = [
+    (value) => !!value || "Password Repeat is required",
+    (value) => value === formState.password || "Passwords doesn't match",
+];
+
 const onAlreadyHavenAnAccountPressed = () => {
     router.push("/login");
+};
+
+const onSubmit = () => {
+    if (!valid.value) {
+        return;
+    }
+
+    console.log("Submit");
 };
 </script>
 
@@ -16,45 +53,56 @@ const onAlreadyHavenAnAccountPressed = () => {
         <v-icon></v-icon>
         <v-card-text class="ma-0 pa-0">
             <v-container>
-                <v-form>
+                <v-form ref="form" v-model="valid" @submit.prevent="onSubmit">
                     <v-text-field
                         id="username"
-                        prepend-icon="mdi-account"
                         name="username"
-                        :label="$t('Username')"
+                        prepend-icon="mdi-account"
                         type="text"
+                        v-model="formState.username"
+                        :label="$t('Username')"
+                        :rules="usernameRules"
                     ></v-text-field>
                     <v-text-field
                         id="email"
-                        prepend-icon="mdi-email"
                         name="email"
-                        :label="$t('Email')"
+                        prepend-icon="mdi-email"
                         type="email"
+                        v-model="formState.email"
+                        :label="$t('Email')"
+                        :rules="emailRules"
                     ></v-text-field>
                     <v-text-field
                         id="password"
-                        prepend-icon="mdi-lock"
                         name="password"
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        v-model="formState.password"
                         :label="$t('Password')"
-                        type="text"
+                        :rules="passwordRules"
                     ></v-text-field>
                     <v-text-field
                         id="passwordRepeat"
-                        prepend-icon="mdi-lock"
                         name="passwordRepeat"
-                        :label="$t('RepeatPassword')"
+                        prepend-icon="mdi-lock"
                         type="password"
+                        v-model="formState.passwordRepeat"
+                        :label="$t('RepeatPassword')"
+                        :rules="passwordRepeatRules"
                     ></v-text-field>
                     <div class="flex flex-row justify-end">
                         <v-btn
-                            variant="outlined"
                             class="mr-4"
+                            variant="outlined"
                             @click="onAlreadyHavenAnAccountPressed"
                             >{{ $t("LoginToAccount") }}</v-btn
                         >
-                        <v-btn variant="elevated" class="self-end">{{
-                            $t("RegisterBtn")
-                        }}</v-btn>
+                        <v-btn
+                            class="self-end"
+                            type="submit"
+                            variant="elevated"
+                            >{{ $t("RegisterBtn") }}</v-btn
+                        >
                     </div>
                 </v-form>
             </v-container>
