f53d654fc6d105410f71e4516e73e8003672f095
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-02-21 22:14:02 +0100

    Added linting and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,36 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
-import i18n from "laravel-vue-i18n/vite";
+import vue from '@vitejs/plugin-vue';
+import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify';
+import eslintPlugin from "vite-plugin-eslint";
+import {defineConfig} from 'vite';
+import {fileURLToPath, URL} from 'node:url';
 
 export default defineConfig({
+    define: {'process.env': {}},
     plugins: [
-        vue(),
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
+        vue({
+            template: {transformAssetUrls}
         }),
-        i18n(),
+        eslintPlugin({cache: false, fix: true}),
+        vuetify({
+            autoImport: true,
+        })
     ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('resources/js', import.meta.url))
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
+    },
+    server: {
+        port: 3000,
+    },
+    base: "./"
 });
