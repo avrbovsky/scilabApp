93fcf14b95eef1f2e2900abbf4384e862c7132da
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-02-21 13:44:29 +0100

    Add plugins folder

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,10 @@
 import "./bootstrap";
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { i18nVue } from "laravel-vue-i18n";
-import { VueQueryPlugin } from "@tanstack/vue-query";
-import piniaPluginPersistedState from "pinia-plugin-persistedstate";
-import VueAxios from "vue-axios";
-import api from "./api/api.js";
-import router from "./router/index.js";
+import {registerPlugins} from "./plugins";
 import App from "./App.vue";
-import vuetify from "./vuetify";
 
 const app = createApp(App);
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedState);
 
-app.use(vuetify);
-app.use(pinia);
-app.use(router);
-app.use(VueAxios, api);
-app.use(VueQueryPlugin);
-app.use(i18nVue, {
-    resolve: async (lang) => {
-        const langs = import.meta.glob("../../lang/*.json");
+registerPlugins(app);
 
-        try {
-            const langModule = await langs[`../../lang/${lang}.json`]();
-            return langModule;
-        } catch (error) {
-            console.error(
-                `Could not load ${lang}, falling back to default language.`
-            );
-            const fallbackLang = "en";
-            const fallbackLangModule = await langs[
-                `../../lang/${fallbackLang}.json`
-            ]();
-            return fallbackLangModule;
-        }
-    },
-});
 app.mount("#app");
---
+++ resources/js/plugins/vuetify.js
@@ -0,0 +1,13 @@
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { md3 } from "vuetify/blueprints";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    blueprint: md3,
+});
+
+export default vuetify;
