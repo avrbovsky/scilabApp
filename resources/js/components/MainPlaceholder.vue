4662a069a4a77e904514f79deca96fdab817c57b
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-02-22 18:40:22 +0100

    Adjust width of main router-view

--- resources/js/components/MainPlaceholder.vue
+++ resources/js/components/MainPlaceholder.vue
@@ -2,10 +2,16 @@
   <v-main>
     <v-container class="h-100 py-5 w-100">
       <v-container
-        class="bg-surface elevation-3 h-100 mx-auto rounded-lg w-75 w-full"
+        class="bg-surface content-container elevation-3 h-100 rounded-lg"
       >
         <slot />
       </v-container>
     </v-container>
   </v-main>
 </template>
+
+<style scoped>
+  .content-container {
+    max-width: 90%;
+  }
+</style>
