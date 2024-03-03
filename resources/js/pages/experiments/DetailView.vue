f9ea501aa5b0eaea8fb15f2d06c5a75eb6a61321
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-03-03 18:44:15 +0100

    Separated Simulate Form to component

--- resources/js/pages/experiments/DetailView.vue
+++ resources/js/pages/experiments/DetailView.vue
@@ -24,29 +24,11 @@
           </div>
         </div>
         <v-divider />
-        <v-form
-          ref="form"
-          class="mt-10"
-          :disabled="isPendingSimulation"
-          validate-on="submit"
-          @submit.prevent="onSubmit"
-        >
-          <v-textarea
-            v-model="experimentInput"
-            label="Input object"
-            prepend-icon="mdi-code-json"
-            :rules="inputRules"
-            variant="outlined"
-          />
-          <div class="d-flex justify-end">
-            <v-btn
-              :disabled="isPendingSimulation"
-              type="submit"
-            >
-              Simulate
-            </v-btn>
-          </div>
-        </v-form>
+        <simulate-form
+          :context="data?.experiment?.context || ''"
+          :loading="isPendingSimulation"
+          :submit="handleSubmit"
+        />
         <v-container>
           <graph-component :data="graphData" />
         </v-container>
@@ -67,6 +49,7 @@ import HeaderComponent from "./components/HeaderComponent.vue";
 import GraphComponent from "./components/GraphComponent.vue";
 import { useAuthStore } from "@/stores/Auth";
 import { storeToRefs } from "pinia";
+import SimulateForm from "./components/SimulateForm.vue";
 
 const route = useRoute();
 const { id } = route.params;
@@ -76,49 +59,16 @@ const { currentLoggedUser } = storeToRefs(authStore);
 const { data, mutateAsync } = useExperimentDetailMutation();
 const { mutateAsync: simulate, isPending: isPendingSimulation } =
     useExperimentSimulateMutation();
-const { showSnackbar } = useNotificationStore();
 
+const { showSnackbar } = useNotificationStore();
 onMounted(async () => {
-    await mutateAsync(id);
-    experimentInput.value = data?.experiment?.context;
+    mutateAsync(id);
 });
 
-const form = ref(null);
-const experimentInput = ref("");
-
-const inputRules = [
-    (value) => isJsonString(value) || "Input is not a valid JSON",
-    (value) =>
-        onlyNumbersAsValue(value) ||
-        "Input must contain only numbers as values",
-];
-
-const isJsonString = (jsonString) => {
-    try {
-        const o = JSON.parse(jsonString);
-
-        if (o && typeof o === "object") {
-            return true;
-        }
-    } catch (e) {
-        /* empty */
-    }
-
-    return false;
-};
-
-const onlyNumbersAsValue = (jsonString) => {
-    const o = JSON.parse(jsonString);
-    const values = Object.values(o);
-    const notNumber = values.find((item) => typeof item !== "number");
-
-    return notNumber === undefined;
-};
-
-const onSubmit = async () => {
+const handleSubmit = async (context) => {
     try {
         const { simulation } = await simulate({
-            context: experimentInput.value,
+            context,
         });
 
         showSnackbar("Experiment simulated successfully", "success");
