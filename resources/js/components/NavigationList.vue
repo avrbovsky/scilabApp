7f2818d96facbc4f633b0d3e1eff6a66580204d4
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-03-08 17:58:54 +0100

    Activate list item when on appropriate page

--- resources/js/components/NavigationList.vue
+++ resources/js/components/NavigationList.vue
@@ -5,12 +5,14 @@
     variant="tonal"
   >
     <v-list-item
+      :active="$route.path === '/experiments/add'"
       href="/#/experiments/add"
       prepend-icon="mdi-plus-circle"
       :title="$t('CreateExperiment')"
       value="addExperiment"
     />
     <v-list-item
+      :active="$route.path === '/experiments'"
       href="/#/experiments"
       prepend-icon="mdi-list-box"
       :title="$t('ExperimentList')"
