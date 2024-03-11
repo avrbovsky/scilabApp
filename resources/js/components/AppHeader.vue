fe57e5e8d7f595e33ffee3e705b8df275a70eb4f
Adrian Vrbovsky <avrbovsky64@gmail.com>
2024-03-11 10:09:26 +0100

    Add dark mode switch

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app-bar elevation="2">
     <template #prepend>
-      <v-app-bar-nav-icon @click.prevent="emit('toggle')" />
+      <v-app-bar-nav-icon
+        variant="tonal"
+        @click.prevent="emit('toggle')"
+      />
     </template>
     <template #title>
       <v-app-bar-title class="font-weight-bold">
